f732d947ad0f400b9a876e1c528cbb2196a91fc6
刘天宇 <lty@liutianyudemacbook-pro.local>
2018-10-28 21:42:12 +0800

    Initial Commit

---
+++ appLaunchScreenAddAd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  appLaunchScreenAddAd
+//
+//  Created by 刘天宇 on 2018/10/28.
+//  Copyright © 2018 lty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ appLaunchScreenAddAd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  appLaunchScreenAddAd
+//
+//  Created by 刘天宇 on 2018/10/28.
+//  Copyright © 2018 lty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
